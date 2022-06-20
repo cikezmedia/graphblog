676b9f1024b0a40227ac19649e4a800e063d7cf1
codewithchuks <victorchikeziec@gmail.com>
2022-06-20 15:41:20 +0100

    new updates

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -7,12 +7,16 @@ const Pagination = ({ items, pageSize, currentPage, onPageChange }) => {
   if (Math.ceil(pageCount) === 1) return null;
   const pages = _.range(1, pageCount + 1);
 
+  const handleNextPage = () => {
+    currentPage = onPageChange(currentPage) + 1;
+  };
+
   return (
     <>
       <div className='flex flex-row justify-between items-center mx-auto'>
         <nav className='flex items-center mx-auto rounded-lg mt-10 mb-10'>
           <button
-            className='h-12 border-2 border-r-0 border-dark-blue
+            className='h-8 border-2 border-r-0 border-dark-blue
                px-4 rounded-l-lg hover:bg-dark-blue hover:text-white'
           >
             <BsArrowLeftShort />
@@ -21,14 +25,15 @@ const Pagination = ({ items, pageSize, currentPage, onPageChange }) => {
             <button
               key={page}
               onClick={() => onPageChange(page)}
-              className={`h-12 border-2 border-r-0 border-dark-blue
+              className={`h-8 border-2 border-r-0 border-dark-blue
           w-12 ${page === currentPage && 'bg-dark-blue text-white'}`}
             >
               {page}
             </button>
           ))}
           <button
-            className='h-12 border-2  border-dark-blue
+            onClick={handleNextPage()}
+            className='h-8 border-2  border-dark-blue
                px-4 rounded-r-lg hover:bg-dark-blue hover:text-white'
           >
             <BsArrowRightShort />
